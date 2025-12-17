51d5e9baff65a4c01bfa1014cfaee826a0c84251
alexcg <alexcomesanagz@gmail.com>
2025-12-17 20:00:55 +0100

    Repositorio avanceClase1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,6 @@
 import Entidades.Evento;
+import Entidades.ParticipaId;
+import Repositorios.*;
 import org.hibernate.Session;
 
 public class Main {
@@ -8,6 +10,14 @@ public class Main {
 
         Session session = HibernateUtil.get().openSession();
 
+        EventoRepositorio eventoRepo = new EventoRepositorio(session);
+        HabilidadRepositorio habilidadRepo = new HabilidadRepositorio(session);
+        ParticipaRepositorio participaRepo = new ParticipaRepositorio(session);
+        PersonajeRepositorio personajeRepositorio = new PersonajeRepositorio(session);
+        TieneHabilidadRepositorio tieneHabilidadRepo = new TieneHabilidadRepositorio(session);
+        TrajeRepositorio trajeRepo = new TrajeRepositorio(session);
+
+        //menu con funciones y pedida de datos
 
         session.close();
         System.out.println("Finalizando la conexion a MySQL");
--- src/main/java/Repositorios/EventoRepositorio.java
+++ src/main/java/Repositorios/EventoRepositorio.java
@@ -1,4 +1,14 @@
 package Repositorios;
 
+
+import org.hibernate.Session;
+
 public class EventoRepositorio {
+    Session session;
+
+    public EventoRepositorio(Session session){
+        this.session = session;
+    }
+
+    //funciones
 }
